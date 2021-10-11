fc52faeecdfc31a2f59b4195e1df9ab26e688a17
areisle <areisle@gmail.com>
2021-10-11 14:39:19 -0700

    feat: change icon url based on if url is bookmarked

--- src/background.ts
+++ src/background.ts
@@ -11,17 +11,55 @@ async function getCurrentUrl() {
 const setIcon = async () => {
     const url = await getCurrentUrl();
     if (url) {
-        const token = await getToken();
-        let iconUrl = "icons/icon-warning-64.png";
-        if (token) {
-            iconUrl = "icons/icon-unbookmarked-64.png";
-        }
+        const iconUrl = await getIconUrl(url);
 
         chrome.action.setIcon({ path: iconUrl }, () => {
             /* ... */
             console.log("icon set", iconUrl);
         });
     }
+};
+
+const getIconUrl = async (url: string): Promise<string> => {
+    try {
+        const token = await getToken();
+
+        if (!token) {
+            return "icons/icon-warning-64.png";
+        }
+
+        const response = await fetch(process.env.API_URL!, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token ?? "",
+            },
+            body: JSON.stringify({
+                query: `
+                    query isBookmarked($url: String!) {
+                        isBookmarked(url: $url)
+                    }
+                `,
+                variables: { url },
+            }),
+        });
+
+        const result = await response.json();
+
+        if (result.errors?.[0]?.extensions?.code === "UNAUTHENTICATED") {
+            return "icons/icon-warning-64.png";
+        }
+
+        if (result.data.isBookmarked) {
+            return "icons/icon-bookmarked-64.png";
+        }
+
+        return "icons/icon-unbookmarked-64.png";
+    } catch (e) {
+        console?.log("error getting is bookmarked", e);
+    }
+
+    return "icons/icon-warning-64.png";
 };
 
 chrome.tabs.onActivated.addListener((arg) => {
