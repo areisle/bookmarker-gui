a0e4cad3f40799be05cff52189364e50152063ed
areisle <areisle@gmail.com>
2021-10-17 11:16:08 -0700

    feat: use dark theme at night and adjust font sizes to be smaller

--- src/theme.tsx
+++ src/theme.tsx
@@ -3,16 +3,47 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { ReactNode } from 'react';
 import { GlobalStyles } from '@mui/material';
 
+const hours = new Date().getHours()
+const isNightTime = hours < 6 || hours > 20
+
 const styles = `
+html {
+    background: ${isNightTime ? 'black' : 'white'};
+    color: ${isNightTime ? 'white' : 'black'};
+}
+
 .MuiLink-root:visited {
-    color: #2b1691;
-    text-decoration-color: #2b1691;
+    color: ${isNightTime ? '#6752cc' : '#2b1691'};
+    text-decoration-color: ${isNightTime ? '#6752cc' : '#2b1691'};
 }
 `;
 
 const inputGlobalStyles = <GlobalStyles styles={styles} />;
 
-const theme = createTheme({
+const createCustomTheme = (prefersDarkMode: boolean) => createTheme({
+    typography: {
+        h1: {
+            fontSize: '2.25rem',
+        },
+        h2: {
+            fontSize: '1.75rem',
+        },
+        h3: {
+            fontSize: '1.5rem',
+        },
+        h4: {
+            fontSize: '1.2rem',
+        },
+        h5: {
+            fontSize: '1.1rem',
+        },
+        h6: {
+            fontSize: '1rem',
+        }
+    },
+    palette: {
+        mode: prefersDarkMode ? 'dark' : 'light',
+    },
     components: {
         MuiInputBase: {
             styleOverrides: {
@@ -84,9 +115,19 @@ const theme = createTheme({
     }
 });
 
-export const CustomThemeProvider = ({ children }: { children: ReactNode }) => (
-    <ThemeProvider theme={theme}>
-        {inputGlobalStyles}
-        {children}
-    </ThemeProvider>
-)
+
+export const CustomThemeProvider = ({ children }: { children: ReactNode }) => {
+    const theme = React.useMemo(
+        () => {
+            return createCustomTheme(isNightTime);
+        },
+        [],
+    );
+
+    return (
+        <ThemeProvider theme={theme}>
+            {inputGlobalStyles}
+            {children}
+        </ThemeProvider>
+    )
+}
