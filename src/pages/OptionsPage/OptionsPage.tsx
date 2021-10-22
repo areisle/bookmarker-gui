65cf6c71535b4e09c17443d1b6268d70af7a5a3c
areisle <areisle@gmail.com>
2021-10-21 22:53:23 -0700

    refactor: flatten out Cateogory component

--- src/pages/OptionsPage/OptionsPage.tsx
+++ src/pages/OptionsPage/OptionsPage.tsx
@@ -1,19 +1,17 @@
 import React, { ReactNode, useState, useLayoutEffect } from 'react';
-import { Button, ListItem, ListItemText, MenuItem, MenuList, Typography } from '@mui/material';
+import { ListItem, ListItemText, MenuItem, MenuList, Tab, Tabs, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import { Category } from '../../components/Category';
+import { CategorySettings } from '../../components/CategorySettings';
 import { useCategories } from '../../queries';
 import { AddCategory } from './AddCategory';
-
-interface Category {
-    id: number;
-    name: string;
-}
+import { TabPanel, tabProps } from '../../components/TabPanel';
+import { BookmarksList } from '../../components/BookmarksList';
 
 function OptionsPage() {
     const { data, error, isLoading: loading } = useCategories({});
 
     const [id, setId] = useState<number | null>(null);
+    const [tab, setTab] = useState(0);
 
     useLayoutEffect(() => {
         if (!id && data?.categories.length) {
@@ -33,7 +31,16 @@ function OptionsPage() {
     } else if (error) {
         content = <Typography color='error'>Unable to retrieve bookmark categories. {error.message}</Typography>
     } else if (id) {
-        content = <Category id={id!} />
+        content = (
+            <>
+                <TabPanel value={tab} index={0}>
+                    <BookmarksList category={id} />
+                </TabPanel>
+                <TabPanel value={tab} index={1}>
+                    <CategorySettings id={id} />
+                </TabPanel>
+            </>
+        )
     } else {
         content = <Typography>No category selected.</Typography>
     }
@@ -63,6 +70,15 @@ function OptionsPage() {
                 </MenuList>
             </Box>
             <Box sx={{ flexGrow: 1 }} component='main'>
+                <Tabs
+                    value={tab}
+                    onChange={(_, nextTab) => setTab(nextTab)}
+                    aria-label="category sub-page"
+                    sx={{ position: 'sticky', top: 0, zIndex: 1, bgcolor: 'background.paper', pl: 1 }}
+                >
+                    <Tab label="Bookmarks" {...tabProps(0)} sx={{ bgcolor: 'background.paper' }} />
+                    <Tab label="Settings" {...tabProps(1)} sx={{ bgcolor: 'background.paper' }} />
+                </Tabs>
                 {content}
             </Box>
         </Box>
