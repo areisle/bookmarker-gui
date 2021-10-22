7ca1d31306267987654542e5b5b8a6647571411b
areisle <areisle@gmail.com>
2021-10-21 22:41:45 -0700

    refactor: move TabPanel to own component folder

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -1,41 +1,8 @@
-import { Tab, Tabs, Typography } from '@mui/material';
-import { Box } from '@mui/system';
+import { Tab, Tabs } from '@mui/material';
 import React, { useState } from 'react';
 import { BookmarksList } from '../BookmarksList';
+import { TabPanel, tabProps } from '../TabPanel';
 import { CategorySettings } from './CategorySettings';
-
-interface TabPanelProps {
-    children?: React.ReactNode;
-    index: number;
-    value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box sx={{ p: 1 }}>
-                    {children}
-                </Box>
-            )}
-        </div>
-    );
-}
-
-function a11yProps(index: number) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-    };
-}
 
 interface CategoryProps {
     id: number;
@@ -52,8 +19,8 @@ function Category(props: CategoryProps) {
                 aria-label="category sub-page"
                 sx={{ position: 'sticky', top: 0, zIndex: 1, bgcolor: 'background.paper', pl: 1 }}
             >
-                <Tab label="Bookmarks" {...a11yProps(0)} sx={{ bgcolor: 'background.paper' }} />
-                <Tab label="Settings" {...a11yProps(1)} sx={{ bgcolor: 'background.paper' }} />
+                <Tab label="Bookmarks" {...tabProps(0)} sx={{ bgcolor: 'background.paper' }} />
+                <Tab label="Settings" {...tabProps(1)} sx={{ bgcolor: 'background.paper' }} />
             </Tabs>
             <TabPanel value={tab} index={0}>
                 <BookmarksList category={id} />
---
+++ src/components/TabPanel/index.tsx
@@ -0,0 +1,1 @@
+export { TabPanel, tabProps } from './TabPanel';
